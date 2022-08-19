7b9277af18e602aeb5aa9f19d255c9ab68c5bb04
junhwan <mynameisljh@naver.com>
2022-08-19 16:32:51 +0900

    띄워쓰기

---
+++ src/main/java/com/example/azt/AztApplication.java
@@ -0,0 +1,13 @@
+package com.example.azt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AztApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AztApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azt/AztApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AztApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
