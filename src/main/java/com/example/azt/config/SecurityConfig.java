edee175ae2aa08fccbe346c5b6b18a218c119526
junhwan <mynameisljh@naver.com>
2022-08-24 22:16:59 +0900

    기타

--- src/main/java/com/example/azt/config/SecurityConfig.java
+++ src/main/java/com/example/azt/config/SecurityConfig.java
@@ -1,27 +1,53 @@
 package com.example.azt.config;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 
+import javax.sql.DataSource;
+
+@RequiredArgsConstructor
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
 
     // 개발단계이기 때문에 security 권한을 모두 풀어줌.
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.authorizeRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin().and().build();
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http    .csrf().disable()
+                        .authorizeRequests()
+                        .anyRequest().permitAll()
+                        .and()
+                    .formLogin()
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login") // post 요청시 보낼 경로
+                        .defaultSuccessUrl("/test")
+                        .permitAll()
+                        .and()
+                    .logout()
+                        .permitAll();
+        }
+
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
-    // 해시 함수를 이용한 암호화 처리
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+
+        // 해시 함수를 이용한 암호화 처리
+        @Bean
+        public BCryptPasswordEncoder encoder() {
+            return new BCryptPasswordEncoder();
+        }
 
 }
--- src/main/java/com/example/azt/domain/constant/UserType.java
+++ src/main/java/com/example/azt/domain/constant/UserType.java
@@ -1,5 +1,14 @@
 package com.example.azt.domain.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserType {
-    USER, ADMIN
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String value;
+
 }
