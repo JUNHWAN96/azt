d9342ce8e318476841fa2b88dddad0a854f1dd6e
junhwan <mynameisljh@naver.com>
2022-08-27 11:37:17 +0900

    페이징 처리

--- src/main/java/com/example/azt/controller/BoardController.java
+++ src/main/java/com/example/azt/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.example.azt.controller;
 import com.example.azt.dto.BoardDto;
 import com.example.azt.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +31,58 @@ public class BoardController {
         boardService.saveBoard(boardDto);
 
         return "redirect:/main/test";
+    }
+
+    @GetMapping("/list")
+    public String boardList(@PageableDefault(size=3, sort="id", direction= Sort.Direction.DESC)Pageable pageable,
+                             Model model) {
+
+        Page<BoardDto> boardDtoList = boardService.findAll(pageable);
+
+        int startPage = Math.max( 1,boardDtoList.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boardDtoList.getTotalPages(),boardDtoList.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("boardDtoList",boardDtoList);
+
+        return "/board/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String boardDetail(@PathVariable Long id, Model model){
+
+       BoardDto board = boardService.detail(id);
+
+       model.addAttribute("board", board);
+
+        return "/board/detail";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable Long id, Model model){
+
+        BoardDto board = boardService.detail(id);
+
+        model.addAttribute("board", board);
+
+        return "/board/update";
+    }
+
+    @PostMapping("/update")
+    public String update( BoardDto boardDto){
+
+        boardService.updateBoard(boardDto);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+
+        boardService.delete(id);
+
+        return "redirect:/board/list";
     }
 
 }
--- src/main/java/com/example/azt/service/BoardService.java
+++ src/main/java/com/example/azt/service/BoardService.java
@@ -3,6 +3,8 @@ package com.example.azt.service;
 import com.example.azt.dto.BoardDto;
 import com.example.azt.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -15,4 +17,64 @@ public class BoardService {
         boardRepository.save(boardDto.toEntity());
     }
 
+    // 게시판 조회
+    public Page<BoardDto> findAll(Pageable pageable) {
+
+        //page 객체로 list를 받아 오는걸로 수정
+
+        return boardRepository.findAll(pageable).map(BoardDto::fromEntity);
+
+//        List<Board> boards = boardRepository.findAll();
+//        List<BoardDto> boardDtoList = new ArrayList<>();
+//
+//        for(Board board : boards ){
+//          BoardDto dto = BoardDto.builder()
+//                       .id(board.getId())
+//                       .title(board.getTitle())
+//                       .hashtag(board.getHashtag())
+//                       .content(board.getContent())
+//                       .writer(board.getWriter())
+//                       .build();
+//
+//          boardDtoList.add(dto);
+//        }
+//        return boardDtoList;
+    }
+
+    // 상세페이지
+    public BoardDto detail(Long id) {
+
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() ->new EntityNotFoundException("게시글이 없습니다"));
+
+        BoardDto dto = BoardDto.builder()
+                      .id(board.getId())
+                      .title(board.getTitle())
+                      .hashtag(board.getHashtag())
+                      .content(board.getContent())
+                      .writer(board.getWriter())
+                      .build();
+
+        return dto;
+    }
+
+    // 게시글 수정
+    public void updateBoard(BoardDto boardDto) {
+
+            Board board = boardRepository.getReferenceById(boardDto.getId());
+
+            board.setTitle(boardDto.getTitle());
+            board.setHashtag(boardDto.getHashtag());
+            board.setContent(boardDto.getContent());
+
+            boardRepository.save(board);
+
+    }
+
+    // 게시글 삭제
+    public void delete(Long id) {
+
+        boardRepository.deleteById(id);
+
+    }
 }
