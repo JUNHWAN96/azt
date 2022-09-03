9dc769d51267972f4aa3769430c462fdaf3466ae
junhwan <mynameisljh@naver.com>
2022-09-03 23:41:27 +0900

    헤더 부분 추출. 로그인 세션 정보 게시글에 입력

--- src/main/java/com/example/azt/config/SecurityConfig.java
+++ src/main/java/com/example/azt/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.example.azt.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록 된다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
+                .antMatchers("/comments/write")
+                .authenticated()
                 .anyRequest()
                 .permitAll()
                 .and()
@@ -22,7 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/auth/login")
                 .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인 진행
                 .usernameParameter("userName")
-                .defaultSuccessUrl("/");
+                .failureHandler(authenticationFailureHandler)
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
 
 
     }
